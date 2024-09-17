c77de828ced106811d8d579c4730a1390df84a04
kofta999 <mostafaxxx555@gmail.com>
2024-09-17 15:35:43 +0300

    Upgrade parser to parse string KV pairs

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,6 +2,10 @@
 pub enum TokenType {
     OpenCurlyBrace,
     CloseCurlyBrace,
+    DoubleQuote,
+    Colon,
+    String,
+    Comma,
 }
 
 #[derive(Debug, PartialEq)]
@@ -10,20 +14,43 @@ pub struct Token {
     pub token_type: TokenType,
 }
 
+fn create_token(value: String, token_type: TokenType) -> Token {
+    Token { value, token_type }
+}
+
 pub fn tokenize(input: &str) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
-    for char in input.chars() {
-        // TODO: Needs change later
+    let mut chars = input.chars();
+    while let Some(char) = chars.next() {
         let token_type = match char {
             '{' => TokenType::OpenCurlyBrace,
             '}' => TokenType::CloseCurlyBrace,
+            '"' => {
+                tokens.push(Token {
+                    value: '"'.to_string(),
+                    token_type: TokenType::DoubleQuote,
+                });
+
+                let mut s = String::new();
+
+                while let Some(c) = chars.next() {
+                    if c == '"' {
+                        break;
+                    } else {
+                        s.push(c);
+                    }
+                }
+
+                tokens.push(create_token(s, TokenType::String));
+
+                TokenType::DoubleQuote
+            }
+            ':' => TokenType::Colon,
+            ',' => TokenType::Comma,
             _ => continue,
         };
 
-        tokens.push(Token {
-            value: char.to_string(),
-            token_type,
-        });
+        tokens.push(create_token(char.to_string(), token_type));
     }
 
     tokens
@@ -36,25 +63,55 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_step_1() {
+    fn test_step_1_lexer() {
         let valid = fs::read_to_string("./tests/step1/valid.json").unwrap();
         let invalid = fs::read_to_string("./tests/step1/invalid.json").unwrap();
 
         assert_eq!(
             vec![
-                Token {
-                    token_type: TokenType::OpenCurlyBrace,
-                    value: "{".to_string()
-                },
-                Token {
-                    token_type: TokenType::CloseCurlyBrace,
-                    value: "}".to_string()
-                },
+                create_token("{".to_string(), TokenType::OpenCurlyBrace),
+                create_token("}".to_string(), TokenType::CloseCurlyBrace),
             ],
             tokenize(&valid)
         );
 
         assert_eq!(Vec::<Token>::new(), tokenize(&invalid));
     }
-}
 
+    #[test]
+    fn test_step2_lexer() {
+        let valid = fs::read_to_string("./tests/step2/valid.json").unwrap();
+        let invalid = fs::read_to_string("./tests/step2/invalid.json").unwrap();
+
+        assert_eq!(
+            vec![
+                create_token("{".to_string(), TokenType::OpenCurlyBrace),
+                create_token('"'.to_string(), TokenType::DoubleQuote),
+                create_token("key".to_string(), TokenType::String),
+                create_token('"'.to_string(), TokenType::DoubleQuote),
+                create_token(":".to_string(), TokenType::Colon),
+                create_token('"'.to_string(), TokenType::DoubleQuote),
+                create_token("value".to_string(), TokenType::String),
+                create_token('"'.to_string(), TokenType::DoubleQuote),
+                create_token("}".to_string(), TokenType::CloseCurlyBrace),
+            ],
+            tokenize(&valid)
+        );
+
+        assert_eq!(
+            vec![
+                create_token("{".to_string(), TokenType::OpenCurlyBrace),
+                create_token('"'.to_string(), TokenType::DoubleQuote),
+                create_token("key".to_string(), TokenType::String),
+                create_token('"'.to_string(), TokenType::DoubleQuote),
+                create_token(":".to_string(), TokenType::Colon),
+                create_token('"'.to_string(), TokenType::DoubleQuote),
+                create_token("value".to_string(), TokenType::String),
+                create_token('"'.to_string(), TokenType::DoubleQuote),
+                create_token(",".to_string(), TokenType::Comma),
+                create_token("}".to_string(), TokenType::CloseCurlyBrace),
+            ],
+            tokenize(&invalid)
+        );
+    }
+}
--- src/parser.rs
+++ src/parser.rs
@@ -3,7 +3,8 @@ use std::cell::RefCell;
 use crate::lexer::{tokenize, Token, TokenType};
 
 pub struct Parser {
-    tokens: RefCell<Box<dyn Iterator<Item = Token>>>,
+    cursor: RefCell<usize>,
+    tokens: Vec<Token>,
 }
 
 impl Parser {
@@ -11,29 +12,66 @@ impl Parser {
         let tokens = tokenize(input);
 
         Parser {
-            tokens: RefCell::new(Box::new(tokens.into_iter())),
+            cursor: RefCell::new(0),
+            tokens,
         }
     }
 
-    pub fn parse(&self) -> Result<(), &str> {
-        match self.tokens.borrow_mut().next() {
+    fn at(&self) -> Result<&Token, &str> {
+        match self.tokens.get(*self.cursor.borrow()) {
+            Some(t) => Ok(t),
+            None => Err("Invalid JSON"),
+        }
+    }
+
+    fn eat(&self) -> Result<&Token, &str> {
+        let mut cursor = self.cursor.borrow_mut();
+        match self.tokens.get(*cursor) {
             Some(t) => {
-                if t.token_type == TokenType::OpenCurlyBrace {
-                } else {
-                    return Err("Invalid JSON");
-                }
+                *cursor += 1;
+                Ok(t)
             }
-            None => return Err("Invalid JSON"),
+            None => Err("Invalid Token"),
+        }
+    }
+
+    fn expect(&self, token_type: TokenType) -> Result<(), &str> {
+        if self.eat()?.token_type != token_type {
+            return Err("Unexpected Token");
         }
 
-        match self.tokens.borrow_mut().next() {
-            Some(t) => {
-                if t.token_type == TokenType::CloseCurlyBrace {
-                } else {
-                    return Err("Invalid JSON");
-                }
-            }
-            None => return Err("Invalid JSON"),
+        Ok(())
+    }
+
+    pub fn parse(&self) -> Result<(), &str> {
+        self.expect(TokenType::OpenCurlyBrace)?;
+        // Do parsing for content
+
+        if self.at()?.token_type == TokenType::DoubleQuote {
+            self.parse_kv_pair()?;
+        }
+
+        self.expect(TokenType::CloseCurlyBrace)?;
+        Ok(())
+    }
+
+    fn parse_kv_pair(&self) -> Result<(), &str> {
+        self.eat()?;
+
+        self.expect(TokenType::String)?;
+        self.expect(TokenType::DoubleQuote)?;
+
+        println!("here");
+
+        self.expect(TokenType::Colon)?;
+
+        self.expect(TokenType::DoubleQuote)?;
+        self.expect(TokenType::String)?;
+        self.expect(TokenType::DoubleQuote)?;
+
+        if self.at()?.token_type == TokenType::Comma {
+            self.eat()?;
+            self.parse_kv_pair()?;
         }
 
         Ok(())
@@ -47,21 +85,37 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parser_step1_valid() {
+    fn test_parser_step1() {
         let valid = fs::read_to_string("./tests/step1/valid.json").unwrap();
 
-        // TODO: fix so that using mut is not needed
         let parser1 = Parser::new(&valid);
 
         assert!(parser1.parse().is_ok());
-    }
 
-    #[test]
-    fn test_parser_step1_invalid() {
         let invalid = fs::read_to_string("./tests/step1/invalid.json").unwrap();
 
         let parser2 = Parser::new(&invalid);
 
         assert!(parser2.parse().is_err());
+    }
+
+    #[test]
+    fn test_parser_step2() {
+        let valid = fs::read_to_string("./tests/step2/valid.json").unwrap();
+        let invalid = fs::read_to_string("./tests/step2/invalid.json").unwrap();
+        let valid2 = fs::read_to_string("./tests/step2/valid2.json").unwrap();
+        let invalid2 = fs::read_to_string("./tests/step2/invalid2.json").unwrap();
+
+        let parser = Parser::new(&valid);
+        assert!(parser.parse().is_ok());
+
+        let parser = Parser::new(&invalid);
+        assert!(parser.parse().is_err());
+
+        let parser = Parser::new(&valid2);
+        assert!(parser.parse().is_ok());
+
+        let parser = Parser::new(&invalid2);
+        assert!(parser.parse().is_err());
     }
 }
